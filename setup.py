3e65bb502780d3fc4bc9fed10f4908dbda3540e6
Rob Gietema <rob@fourdigits.nl>
2010-08-26 10:37:07 +0000

    Initial checkin xdv theme.
    svn path=/decobrewery/decobrewery.xdvtheme/trunk/; revision=124208

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='decobrewery.xdvtheme',
+      version=version,
+      description="The Deco Brewery XDV Theme",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Rob Gietema',
+      author_email='rob@fourdigits.nl',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['decobrewery'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.xdv',
+      ],
+      entry_points="""
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
